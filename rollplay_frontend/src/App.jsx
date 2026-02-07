869cfa2f27c07f7db4472bbefadbce24755cfb54
chara1 <saket.nimkar@gmail.com>
2026-02-07 01:45:46 +0000

    Ignore
    Removing public session token.
    readying up for matching.

--- rollplay_frontend/src/App.jsx
+++ rollplay_frontend/src/App.jsx
@@ -25,36 +25,34 @@ import TipToss from "./pages/TipToss";
 
 
 export default function App() {
-  const [token, setToken] = useState("eyJhbGciOiJIUzI1NiIsImtpZCI6IkNyMDh3UUZuczlZSmN6VW8iLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3FvcWl0aGJwdmh2cmxscmNwcmpoLnN1cGFiYXNlLmNvL2F1dGgvdjEiLCJzdWIiOiI3ZjE3ZGUxZC1mNmM2LTRiMTUtYWQyNC03YmQ0MjIyYTRiNDgiLCJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzY0MzEzMTA5LCJpYXQiOjE3NjQzMDk1MDksImVtYWlsIjoibGxzQGV4YW1wbGUuY29tIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJlbWFpbCIsInByb3ZpZGVycyI6WyJlbWFpbCJdfSwidXNlcl9tZXRhZGF0YSI6eyJlbWFpbCI6Imxsc0BleGFtcGxlLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaG9uZV92ZXJpZmllZCI6ZmFsc2UsInN1YiI6IjdmMTdkZTFkLWY2YzYtNGIxNS1hZDI0LTdiZDQyMjJhNGI0OCJ9LCJyb2xlIjoiYXV0aGVudGljYXRlZCIsImFhbCI6ImFhbDEiLCJhbXIiOlt7Im1ldGhvZCI6InBhc3N3b3JkIiwidGltZXN0YW1wIjoxNzY0MzA5NTA5fV0sInNlc3Npb25faWQiOiI1NWE2NmMxMC1mZjJmLTQzNjMtYjFhMy01Zjk5MThjMGI2YmIiLCJpc19hbm9ueW1vdXMiOmZhbHNlfQ.haAV_yYZ-7OcC4q7iCy-3OTHbMbtoqSa-wse_XqoWSY");
+  const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
 
-  // this is saket
-  // useEffect(() => {
-  //   const { data: authListener } = supabase.auth.onAuthStateChange(
-  //     async (_event, session) => {
-  //       if (session?.access_token) {
-  //         setToken(session.access_token);
-  //         setUser(session.user);
-  //       } else {
-  //         setToken(null);
-  //         setUser(null);
-  //       }
-  //     }
-  //   );
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      async (_event, session) => {
+        if (session?.access_token) {
+          setToken(session.access_token);
+          setUser(session.user);
+        } else {
+          setToken(null);
+          setUser(null);
+        }
+      }
+    );
 
-  //   // Load initial session on page refresh
-  //   supabase.auth.getSession().then(({ data }) => {
-  //     if (data.session) {
-  //       setToken(data.session.access_token);
-  //       setUser(data.session.user);
-  //     }
-  //   });
+    // Load initial session on page refresh
+    supabase.auth.getSession().then(({ data }) => {
+      if (data.session) {
+        setToken(data.session.access_token);
+        setUser(data.session.user);
+      }
+    });
 
-  //   return () => {
-  //     authListener.subscription.unsubscribe();
-  //   };
-  // }, []);
-  // this is saket
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+  }, []);
 
   return (
     <GameProvider>
